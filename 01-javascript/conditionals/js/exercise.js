88f969959cef22abbafc8eebfeacf294a65c65d9
William of Ockham <certainthings@gmail.com>
2017-10-25 12:11:44 +1100

    solutions to conditional exercises

---
+++ 01-javascript/conditionals/js/exercise.js
@@ -0,0 +1,93 @@
+// What number's bigger?
+//
+// Write a function named greaterNum that:
+//
+// takes 2 arguments, both numbers.
+// returns whichever number is the greater (higher) number.
+// Call that function 2 times with different number pairs, and log the output to make sure it works (e.g. "The greater number of 5 and 10 is 10.").
+
+const greaterNum = function (a, b) {
+  if (a > b) {
+    return a;
+  } else {
+    return b;
+  }
+};
+
+console.log(`The greater number of 5 and 10 is ${ greaterNum(5, 10) }.`);
+console.log(`The greater number of 10 and -15 is ${ greaterNum(10, -15) }.`);
+
+// The World Translator
+//
+// Write a function named helloWorld that:
+//
+// takes 1 argument, a language code (e.g. "es", "de", "en")
+// returns "Hello, World" for the given language, for atleast 3 languages. It should default to returning English.
+// Call that function for each of the supported languages and log the result to make sure it works.
+
+const helloWorld = function (code) {
+  if ('es' === code) {
+    return 'Hola mundo';
+  } else if ('de' === code) {
+    return 'Hallo Welt';
+  } else { // Default is en
+    return 'Hello world';
+  }
+}
+
+console.log( helloWorld('es') );
+console.log( helloWorld('de') );
+console.log( helloWorld('en') );
+console.log( helloWorld('jp') );
+
+
+// The Grade Assigner
+//
+// Write a function named assignGrade that:
+//
+// takes 1 argument, a number score.
+// returns a grade for the score, either "A", "B", "C", "D", or "F".
+// Call that function for a few different scores and log the result to make sure it works.
+// The Pluralizer
+//
+
+const assignGrade = function (score) {
+  if (score >= 90) {
+    return 'A';
+  } else if (score >= 80) {
+    return 'B';
+  } else if (score >= 70) {
+    return 'C';
+  } else if (score >= 60) {
+    return 'D';
+  } else {
+    return 'F';
+  }
+ }
+
+
+ console.log(`For the score of 100 you get ${ assignGrade(100) }`);
+ console.log(`For the score of 90 you get ${ assignGrade(90) }`);
+ console.log(`For the score of 85 you get ${ assignGrade(85) }`);
+ console.log(`For the score of 70 you get ${ assignGrade(70) }`);
+ console.log(`For the score of 50 you get ${ assignGrade(50) }`);
+
+
+// Write a function named pluralize that:
+//
+// takes 2 arguments, a noun and a number.
+// returns the number and pluralized form, like "5 cats" or "1 dog".
+// Call that function for a few different scores and log the result to make sure it works.
+// Bonus: Make it handle a few collective nouns like "sheep" and "geese".
+
+const pluralize = function (noun, number) {
+  if (1 === number) {
+    return `${ number } ${ noun }`;
+  } else {
+    return `${ number } ${ noun }s`;
+  }
+}
+
+console.log( pluralize('cat', 5) );
+console.log( pluralize('dog', 1) );
+console.log( pluralize('dog', 0) );
