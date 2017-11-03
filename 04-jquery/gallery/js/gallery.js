1e57c447717956bde9d60167332e376403325de5
JT <certainthings@gmail.com>
2017-11-03 12:51:41 +1100

    funtext / thanks bill

--- 04-jquery/gallery/js/gallery.js
+++ 04-jquery/gallery/js/gallery.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+
+  $('h1').funText(500, 'reverseCandy');
+  $('a').funText(100, 'rainbow');
+
   $('img.thumbnail').on('click', function () {
     const imageSrc = $(this).attr('src'); // Getter
     $('#full').hide().attr('src', imageSrc).fadeIn(800); // Setter
   });
+
 });
