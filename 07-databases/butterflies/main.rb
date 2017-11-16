bef4011f90d7d32916da15571f7ebc9c62f5fa27
JT <certainthings@gmail.com>
2017-11-16 15:10:24 +1100

    yay full butterfly crud with hellish sql

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -30,6 +30,19 @@ get '/butterflies/:id' do
   erb :butterflies_show
 end
 
+# Edit: Shows the existing values for a single butterfly for editing.
+get '/butterflies/:id/edit' do
+  @butterfly = query_db('SELECT * FROM butterflies WHERE id = ' + params[:id]).first
+  erb :butterflies_edit
+end
+
+# Update: Updates an existing butterfly in the database with new information.
+post '/butterflies/:id' do
+  update = "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id = #{ params[:id] }"
+  query_db update
+  redirect to("/butterflies/#{ params[:id] }")
+end
+
 # Destroy: Deletes the butterfly with the provided ID from the database
 get '/butterflies/:id/delete' do
   query_db 'DELETE FROM butterflies WHERE id = ' + params[:id]
