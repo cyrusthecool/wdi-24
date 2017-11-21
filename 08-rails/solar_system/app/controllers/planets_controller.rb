f9b299cec877e676feb17c949411ba15a614e212
JT <certainthings@gmail.com>
2017-11-21 15:26:41 +1100

    pretty dismal looking but complete planet CRUD site in rails

--- 08-rails/solar_system/app/controllers/planets_controller.rb
+++ 08-rails/solar_system/app/controllers/planets_controller.rb
@@ -22,4 +22,21 @@ class PlanetsController < ApplicationController
 
     redirect_to planets_path
   end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def update
+    planet = Planet.find params[:id]
+    planet.update :name => params[:name], :image => params[:image], :orbit => params[:orbit], :diameter => params[:diameter], :mass => params[:mass], :moons => params[:moons]
+
+    redirect_to planet_path(planet.id)
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path
+  end
 end
--- 08-rails/solar_system/config/routes.rb
+++ 08-rails/solar_system/config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get '/planets/new' => 'planets#new' # Shows the form for a new planet
   post '/planets' => 'planets#create' # Shoves the new planet into the db
   get '/planets/:id' => 'planets#show', :as => 'planet' # Shows a single planet
+  get '/planets/:id/edit' => 'planets#edit', :as => 'planet_edit'
+  post '/planets/:id' => 'planets#update'
+  delete '/planets/:id' => 'planets#destroy'
 end
+
+# C new create
+# R index show
+# U edit update
+# D delete
