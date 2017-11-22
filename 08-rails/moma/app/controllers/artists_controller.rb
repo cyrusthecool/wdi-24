96568f26054a6c4dc36bcfb1e55f7b0bd428d41d
JT <certainthings@gmail.com>
2017-11-22 15:05:52 +1100

    artist destroy/edit/update

--- 08-rails/moma/app/controllers/artists_controller.rb
+++ 08-rails/moma/app/controllers/artists_controller.rb
@@ -13,10 +13,23 @@ class ArtistsController < ApplicationController
   end
 
   def edit
+    @artist = Artist.find params[:id]
+  end
+
+  def update
+    artist = Artist.find params[:id]
+    artist.update artist_params
+    redirect_to artist
   end
 
   def show
     @artist = Artist.find params[:id]
+  end
+
+  def destroy
+    artist = Artist.find params[:id]
+    artist.destroy
+    redirect_to artists_path
   end
 
   private
--- 08-rails/moma/app/controllers/works_controller.rb
+++ 08-rails/moma/app/controllers/works_controller.rb
@@ -4,6 +4,12 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
+  end
+
+  def create
+    work = Work.create work_params
+    redirect_to work
   end
 
   def edit
@@ -11,5 +17,10 @@ class WorksController < ApplicationController
 
   def show
     @work = Work.find params[:id]
+  end
+
+  private
+  def work_params
+    params.require(:work).permit(:title, :year, :medium, :style, :image)
   end
 end
