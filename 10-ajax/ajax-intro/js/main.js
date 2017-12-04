e2caf133a9c1d0a1494ae68f6fdac99d42b27211
JT <certainthings@gmail.com>
2017-12-04 14:31:34 +1100

    number facts number facts number facts

--- 10-ajax/ajax-intro/js/main.js
+++ 10-ajax/ajax-intro/js/main.js
@@ -1,9 +1,25 @@
-setInterval(function () {
+const fetchFact = function () {
   const xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
-    if (xhr.readyState !== 4) return;
-    document.body.innerHTML += xhr.response; // TODO: Make this better with actual HTML
+    console.log('readyState', xhr.readyState);
+    // Ignore all the readyStates except 4 (i.e. completed/done)
+    if (xhr.readyState !== 4) {
+      return;
+    }
+
+    const info = JSON.parse( xhr.response ); // Convert the string object into a real object.
+
+    const p = document.createElement('p');
+    p.innerHTML = `<strong>${ info.number }</strong>: ${ info.text }`; // Template
+    document.body.appendChild( p );
   }
-  xhr.open('GET', 'http://numbersapi.com/random/trivia');
+  xhr.open('GET', 'http://numbersapi.com/random/trivia?json');
   xhr.send(); // Asynchronous
-}, 5000);
+
+  console.log('fetchFact is done');
+};
+
+fetchFact();
+
+const button = document.getElementsByTagName('button')[0];
+button.addEventListener('click', fetchFact);
