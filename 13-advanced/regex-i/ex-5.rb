b57f81d7175afa7bd402087e389ca1cfdb07230b
JT <certainthings@gmail.com>
2018-01-08 14:21:23 +1100

    rake morning and afternoon

---
+++ 13-advanced/regex-i/ex-5.rb
@@ -0,0 +1,9 @@
+# Make a program that prints each line that has a two of the same nonwhitespace characters next to each other. It should match lines that contain words such as Mississippi, Bamm-Bamm, or llama.
+
+# Back references
+# Create with ()
+# Refer to the capture with \1
+
+ARGF.each do |line|
+  puts line if line =~ /(.)\1/
+end
