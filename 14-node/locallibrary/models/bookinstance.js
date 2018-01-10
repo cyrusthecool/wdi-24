65b5e4bd3cd033301dff6da43728ac1204a4ba5b
JT <certainthings@gmail.com>
2018-01-10 13:01:23 +1100

    library with express and mongoose

---
+++ 14-node/locallibrary/models/bookinstance.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.ObjectId, ref: 'Book', required: true },
+    imprint: { type: String, required: true },
+    status: { type: String, required: true, enum: ['Available', 'Loaned', 'Reserved'], default: 'Available' },
+    due_back: { type: Date, default: Date.now }
+  }
+);
+
+BookInstanceSchema.virtual('url').get(function () {
+  return '/catalog/bookinstance/' + this._id;
+});
+
+module.exports = mongoose.model('BookInstance', BookInstanceSchema);
