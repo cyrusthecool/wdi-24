f985b6c40e547e7b7c227ddf1a63e04aec48363f
JT <certainthings@gmail.com>
2018-01-11 12:03:31 +1100

    more or less finished paintr

--- 13-advanced/paintr/app/assets/javascripts/paintr.js
+++ 13-advanced/paintr/app/assets/javascripts/paintr.js
@@ -4,4 +4,21 @@ $(document).ready(function () {
     const $swatch = $('<div />').addClass('swatch').css('background-color', color);
     $swatch.appendTo('.palette');
   });
+
+  // Event delegation
+  // We can listen for events on elements that don't exist when the page is loaded.
+  $('.palette').on('click', '.swatch', function () {
+    $('.selected').removeClass('selected'); // Prevent having multiple swatches selected.
+    $(this).addClass('selected');
+    console.log('new color selected');
+  });
+
+  // Here event delegation reduces the number of event handlers required in the browser.
+  $('.canvas').on('mouseover', '.pixel', function (event) {
+
+    if (event.shiftKey) return;
+
+    const color = $('.swatch.selected').css('background-color');
+    $(this).css('background-color', color);
+  });
 });
