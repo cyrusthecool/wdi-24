72182d48110ba96c48fc06d22060c1d54cdf964c
JT <certainthings@gmail.com>
2018-01-17 15:57:45 +1100

    fast recursive fibonacci

--- 13-advanced/recursion/fibonacci.rb
+++ 13-advanced/recursion/fibonacci.rb
@@ -19,5 +19,18 @@ def fibonacci_r(n)
   end
 end
 
+def fib(n, a=1, b=1)
+  if n <= 2
+    b # Base case
+  else
+    fib(n - 1, b, a+b) # SICP
+  end
+end
+
+# Homework TODO
+# Write a faster recursive fibonacci
+# 1. Memoisation
+# 2. Make a single recursive call each time
+
 require 'pry'
 binding.pry
